9e358b60ffbd3600c92ba866f544e93511eab811
QYKing233 <61415390+QYKing233@users.noreply.github.com>
2022-10-16 10:42:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -84,22 +84,3 @@ wget -P target/linux/rockchip/armv8/base-files/usr/bin/ https://github.com/orang
 
 # 替换默认主题为 luci-theme-argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
-
-
-# 添加zsh
-mkdir -p files/root
-pushd files/root
-
-## Install oh-my-zsh
-# Clone oh-my-zsh repository
-git clone https://github.com/robbyrussell/oh-my-zsh ./.oh-my-zsh
-
-# Install extra plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ./.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ./.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-completions ./.oh-my-zsh/custom/plugins/zsh-completions
-
-# Get .zshrc dotfile
-cp $GITHUB_WORKSPACE/.zshrc .
-
-popd
