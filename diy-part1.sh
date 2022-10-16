066805ff214be290cbd6c79f0f8c08cb817c9d20
QYKing233 <61415390+QYKing233@users.noreply.github.com>
2022-10-16 13:15:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,9 +8,7 @@
 #=================================================
 
 # alist
-git clone https://github.com/sbwml/luci-app-alist package/alist
-rm -rf feeds/packages/lang/golang
-svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
+
 
 # Clone community packages to package/community
 mkdir package/community
