b6ffe72968116017206ee46223f50887dff45717
QYKing233 <61415390+QYKing233@users.noreply.github.com>
2022-10-17 21:25:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,7 +8,73 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
 
-#add passwall
-m
+#### Clone community packages to package/community
+mkdir package/community
+pushd package/community
+
+# Add luci-app-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall.git
+svn export https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall
+
+
+# Add luci-app-ssr-plus
+git clone --depth=1 https://github.com/fw876/helloworld.git
+
+
+# Add luci-app-vssr
+git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb.git
+git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr.git
+
+
+# Add OpenClash
+svn export https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
+
+
+# Add luci-app-onliner (need luci-app-nlbwmon)
+git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner.git
+
+
+# Add luci-app-poweroff
+git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff.git
+
+
+# Add luci-app-adguardhome
+svn export https://github.com/Boos4721/OpenWrt-Packages/trunk/luci-app-adguardhome
+
+
+# Add luci-app-argon-config & luci-app-argon 
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git
+
+
+# Add open-app-filter
+git clone --depth=1 https://github.com/destan19/OpenAppFilter.git
+
+
+# Add luci-theme-neobird
+git clone --depth=1 https://github.com/thinktip/luci-theme-neobird.git
+
+
+# Add luci-app-syncthing
+svn export https://github.com/kenzok8/small-package/trunk/luci-app-syncthing
+
+
+# Add luci-app-filebrowser
+svn export https://github.com/kenzok8/small-package/trunk/luci-app-filebrowser
+
+
+# Add luci-app-fileassistant
+svn export https://github.com/kenzok8/small-package/trunk/luci-app-fileassistant
+
+
+# Add luci-app-beardropper
+svn export https://github.com/kenzok8/small-package/trunk/luci-app-beardropper
+
+
+#Add luci-app-aliddns
+svn export https://github.com/kenzok8/small-package/trunk/luci-app-aliddns
+
+
+### exit package/community
+popd
